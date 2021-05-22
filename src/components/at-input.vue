f12a9b1b4950b04830bc0af1bbde4dde80110909
josephvong1008 <m15914375662@163.com>
2021-05-22 15:39:12 +0800

    202105221539

--- src/components/at-input.vue
+++ src/components/at-input.vue
@@ -1,36 +1,25 @@
 <template>
-  <div>
-     <el-input class="at-input" :size="type === 'textarea' ? 'small' : size" v-model="useless">
-       <template slot="append"> 
-        <!-- <div ref="shadowDom" :class="['content-shadow', 'el-input__inner', `${type}-type`]" style="border: 1px solid; display:inline-block;height: auto"> 
-        </div> -->
-          <el-popover
-          placement="right-end"
-          title="标题"
-          width="400"
-          trigger="manual"
-           
-          v-model="listVisible"> 
-            <div>
-              <ul>
-                <li v-for="i in 5" @click="itemClick(i)" :key="i">{{i}}</li>
-              </ul>
-            </div>
-            <div  slot="reference" class="popup-btn" :style="{'left':`${popupLeft}px`, 'top': `${popupTop}px`}"></div>
-          </el-popover>
-       
-        <div ref="contentEdit" :class="['content-edit', 'el-input__inner', `${type}-type`]"  :contenteditable="true" @keydown="editHandle" :style="contentEditStyle"></div>
-        <!-- <div v-show="listVisible" class="popup-warp">
+   
+  <el-input class="at-input" :size="type === 'textarea' ? 'small' : size" v-model="useless">
+    <template slot="append"> 
+      <el-popover
+      v-bind="{...popoverProps}"
+      trigger="manual" 
+      v-model="listVisible"> 
+        <slot name="popover"></slot>
+        <!-- <div>
           <ul>
-            <li>a</li>
-            <li>b</li>
-            <li>c</li>
+            <li v-for="i in 5" @click="itemClick(i)" :key="i">{{i}}</li>
           </ul>
         </div> -->
-       </template>
-     </el-input>
-
-  </div>
+        <div  slot="reference" class="popup-btn" :style="{'left':`${popupLeft}px`, 'top': `${popupTop}px`}"></div>
+      </el-popover>
+    
+    <div ref="contentEdit"  :class="['content-edit', 'el-input__inner', `${type}-type`]"  :contenteditable="true" @keydown="editHandle"  :style="{'max-width': `${maxWidth}px`, ...contentEditStyle}"></div>
+    
+    </template>
+  </el-input>
+ 
 </template>
 <style lang="scss" scoped>
 .at-input.el-input{
@@ -45,14 +34,19 @@
     border-radius: 0px;
     padding: 0px;
     width: 100%; 
+    max-width:100%;
     & > .el-input__inner{
       display: block;
+      border-radius: 4px;
+      width: 100%; 
+      max-width:100%;
     }
     .content-edit{
       &.textarea-type{
         overflow: auto;
       }
-      .input-type{ 
+      &.input-type{
+        overflow: hidden; 
         div{
           display: inline;
         }
@@ -67,38 +61,12 @@
         }
       }
     }
-    /*
-    .content-shadow{
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: auto;
-      max-width: 100%;
-      background: none;
-      color: rgba(0,0,0,0.4); 
-      pointer-events: none;
-      opacity: 0.1; 
-      &.input-type{
-        div{
-          display: inline;
-        }
-        br {
-          display: none
-        }
-      }
-      &.textarea-type{
-        div{
-          width:fit-content;
-          border-bottom: 1px solid;
-        }
-      }
-    }
-    */
+     
     .popup-btn{
       position: fixed;
-      width: 10px;
-      height: 10px;
-      background: red;
+      width: 3px;
+      height: 3px;
+      pointer-events: none;
     }
      
   }
@@ -111,10 +79,26 @@
 </style>
 <script>
 const TextareaLineHeight = 36
+const TagClassName = 'tag-span'
+const DefaultPopoverProps = {
+  placement: 'right-end',
+  title: '标题2',
+  width: 400
+}
 export default {
   mixins: [],
   components: {},
   props: {
+    tagClassName: {
+      type: String,
+      default: TagClassName
+    },
+    popoverProps: {
+      type: Object,
+      default: () => {
+        return DefaultPopoverProps
+      }
+    },
     type: {
       type: String,
       default: 'input',
@@ -132,10 +116,17 @@ export default {
       validator: function(v) {
         return ['medium', 'small', 'mini'].includes(v)
       }
+    },
+    preInsert: {
+      type: Function,
+      default: () => {
+        return Promise.reject(new Error('未定义插入数据'))
+      }
     }
   },
   data() {
     return {
+      maxWidth: 280,
       useless: '',
       listVisible: false,
       lastSelection: {
@@ -146,8 +137,12 @@ export default {
       },
       popupLeft: 0,
       popupTop: 0,
-      maxEditHeight: TextareaLineHeight
-      // editWarpHeight: this.height || 0
+      maxEditHeight: TextareaLineHeight,
+
+      tagData: {},
+
+      editContent: '',
+      contentChangeTimer: null
     }
   },
   computed: {
@@ -155,8 +150,13 @@ export default {
       if (this.type === 'textarea') {
         return {height: `${TextareaLineHeight * this.rows}px`} 
       } else {
-        return null
+        return {}
       }
+    }
+  },
+  watch: {
+    editContent(val) {
+      this.$emit('change', val)
     }
   },
   methods: {
@@ -166,27 +166,46 @@ export default {
       this.lastSelection.selection = selection
       this.lastSelection.lastOffset = selection.focusOffset
       
-      this.listVisible = false
+      // this.listVisible = false
       
 
       if (ev.code === 'Digit2' && ev.shiftKey) {  
         this.lastSelection.offset = selection.focusOffset
         setTimeout(() => {
           this._atKeyHandle()
-        }, 200)
+        }, 200) 
+      } 
+      if(ev.code === 'Backspace') {
         
-
+        this._backSpaceHandle()
+      } 
+       
+      if (this.contentChangeTimer) {
+        clearTimeout(this.contentChangeTimer)
       }
+      this.contentChangeTimer = setTimeout(() => {
+        this.editContent = this.$refs.contentEdit.innerHTML 
+      }, 500)
 
-      // if(ev.code === 'Enter' && this.) {}
-      // console.log(ev.target.innerHTML)
+      if (this.listVisible) {
+        const reg = /\s/g
+        setTimeout(() => {
+          const selection = window.getSelection()
+          const atArr = this.lastSelection.range.endContainer.textContent.split('@')
+          const afterAtStr = atArr[atArr.length - 1]
+          this.$emit('on-tagEdit', afterAtStr)
+          if (reg.test(selection.focusNode.nodeValue[selection.focusOffset - 1])) {
+            this.$nextTick(() => {
+              this.listVisible = false
+            })
+          } 
+        }, 50)
+      }
+       
     },
-    // '@' 字符键入时相应函数
-    _atKeyHandle() {
-      
-      
-      const currentTextNode = this.lastSelection.selection.baseNode
-      // console.log(this.lastSelection.selection, currentTextNode, this.lastSelection.lastOffset)
+    // '@' 字符键入时响应函数
+    _atKeyHandle() { 
+      const currentTextNode = this.lastSelection.selection.baseNode 
       const range = document.createRange()
       range.setStart(currentTextNode, 0)
       range.setEnd(currentTextNode, this.lastSelection.lastOffset)
@@ -198,54 +217,97 @@ export default {
       this.popupTop = top
       this.listVisible = true
     },
-
-    itemClick(i) {
-      console.log(i)
+    // 回退删除按键响应函数
+    _backSpaceHandle() {
       const range = this.lastSelection.range 
-      const num = this.lastSelection.lastOffset - this.lastSelection.offset === 0 
-      ? this.lastSelection.lastOffset + 1 : this.lastSelection.lastOffset
-      const textNode = range.startContainer
-      console.log(num, textNode, this.lastSelection.offset, this.lastSelection.lastOffset)
-      range.setStart(textNode, this.lastSelection.offset)
-      range.setEnd(textNode, num)
-      range.deleteContents()
-
-
-
-      const tagSpan = document.createElement('span')
-      const spaceSpan = document.createElement('span')
-
-
-      tagSpan.className = 'tag-span'
-      tagSpan.style.color = 'red'
-      tagSpan.innerHTML = '@' + i
-      spaceSpan.innerHTML = '&nbsp;'
-
-      const fragment = document.createDocumentFragment()
-
-      let node = ''
-      let lastNode = ''
-      fragment.appendChild(tagSpan)
-      while((node = spaceSpan.firstChild)) {
-        lastNode = fragment.appendChild(node)
+      // 删除到'@'前一个字符时，弹框出现 
+      if (range.endContainer.textContent[range.endContainer.textContent.length - 2] === '@') {
+        this.listVisible = true
       }
-      if (this.lastSelection.range && this.lastSelection.selection) {
-         
-        this.lastSelection.range.insertNode(fragment)
-        this.lastSelection.selection.extend(lastNode, 1)
-        this.lastSelection.selection.collapseToEnd()
-      } 
-      this.listVisible = false
+      // 删除到'@'字符时，弹框关闭
+      if (range.endContainer.textContent[range.endContainer.textContent.length - 1] === '@') {
+        this.listVisible = false
+      }
 
+      let removeNode = null
+      if (range.startOffset <= 1 && range.startContainer.parentElement.className !== this.tagClassName) {
+        removeNode = range.startContainer.previousElementSibling
+      }
+    
 
+      if (range.startContainer.parentElement.className === this.tagClassName) {
+        removeNode = range.startContainer.parentElement
+      }
+
+      
+      if (removeNode && removeNode.className === this.tagClassName) { 
+        this.$refs.contentEdit.removeChild(removeNode)
+
+        this.$emit('on-tagDelete', this.tagData[removeNode.id])
+        delete this.tagData[removeNode.id] 
+      }
+    },
+     
+    tagInsert(data) {
+      if (data) {
+        const range = this.lastSelection.range 
+        const num = this.lastSelection.lastOffset - this.lastSelection.offset === 0 
+        ? this.lastSelection.lastOffset + 1 : this.lastSelection.lastOffset
+        const textNode = range.startContainer
+        
+        range.setStart(textNode, this.lastSelection.offset)
+        range.setEnd(textNode, num)
+        range.deleteContents() 
+
+        const tagSpan = document.createElement('span')
+        const spaceSpan = document.createElement('span')
+
+        // 设置标签
+        tagSpan.className = this.tagClassName
+        tagSpan.style.color = data.color || '#409EFF'
+        tagSpan.innerHTML = '@' + data.name
+        spaceSpan.innerHTML = '&nbsp;'
+        
+        // 绑定数据
+        const tagId = data.id ? data.id : `tag_${new Date().getTime()}`
+        tagSpan.id = tagId
+        this.tagData[tagId] = data.data
+
+        const fragment = document.createDocumentFragment()
+
+        let node = ''
+        let lastNode = ''
+        fragment.appendChild(tagSpan)
+        while((node = spaceSpan.firstChild)) {
+          lastNode = fragment.appendChild(node)
+        }
+        if (this.lastSelection.range && this.lastSelection.selection) {
+          
+          this.lastSelection.range.insertNode(fragment)
+          this.lastSelection.selection.extend(lastNode, 1)
+          this.lastSelection.selection.collapseToEnd()
+        } 
+        this.listVisible = false 
+      } else {
+        this.$message.danger('未设置绑定数据')
+        this.listVisible = false
+      }
+
+    },
+    _setMaxWidth() {
+      this.maxWidth = this.$el.offsetWidth
     }
   },
   created() {},
   mounted() {
-    console.log(this._uid)
     setTimeout(() => {
+      this._setMaxWidth()
       this.maxEditHeight = this.$refs.contentEdit.offsetHeight
     }, 300)
+
+    
+    
+
   }
 }
 </script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,51 @@
 <template>
-  <div >
-    work 
+  <div style="padding: 40px">
+    <div style="width: 280px">
+      <at-input type="input" ref="atInput">
+        <div slot="popover">
+          <ul>
+            <li v-for="item in userList" :key="item.id" @click="listClick(item)">{{item.name}}</li>
+          </ul>
+        </div>
+      </at-input>
+    </div>
+    
+    <!-- @change="contentChange" @on-tagEdit="tagEditChange" -->
   </div>
 </template>
 <script>
-export default {}
+import AtInput from '@/components/at-input'
+export default {
+  components: {
+    AtInput
+  },
+  data() {
+    return {
+      userList: [
+        {name: '黄吉平', id: 'huangjiping'},
+        {name: '李国师', id: 'liguoshi'},
+        {name: '寇永明', id: 'kouyongming'},
+        {name: '潘浩玮', id: 'panhaowei'}
+
+      ]
+    }
+  },
+  methods: {
+    tagEditChange(val) {
+      console.log('tag', val)
+    },
+    contentChange(val) {
+      console.log('content', val)
+    },
+    listClick(data) {
+      this.$refs.atInput.tagInsert({
+        name: data.name,
+        data: data,
+        color: 'red'
+      })
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
 
