34a64ca5dd17f43a05a9114a06890e047bafee16
josephvong1008 <m15914375662@163.com>
2021-06-02 18:28:51 +0800

    添加tree-select仅选末端节点功能与代码优化

---
+++ src/views/modify-test/tree-select.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  添加 ‘only-leaf’ 配置 控制树桩选择器仅允许选择最末端节点。
+  <my-form :model="model" @submit="handleSubmit">
+    <my-tree-select name="tree" label="下拉树" :options="options" :props="{'multiple': true, 'clearable': true, 'only-leaf': true}">
+    </my-tree-select>
+  </my-form>
+
+</div>
+</template>
+
+<script>
+  import tree from '../../../docs/.vuepress/data/tree.json'
+
+  export default {
+    data() {
+      return {
+        options: tree,
+        model: {
+          tree: 'yizhi'
+        }
+      }
+    },
+    methods: {
+       
+      handleSubmit(model) {
+        alert(`submit: ${JSON.stringify(model)}`)
+      }
+    }
+  }
+</script>
