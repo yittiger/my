ca81b907f0967f4a87dad9c8d1f4afb73e0d0785
josephvong1008 <m15914375662@163.com>
2021-08-29 11:42:37 +0800

    202108291200

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,15 +1,15 @@
 <template>
 <div>
-  <my-form ref="form" :model="model" @submit="handleSubmit" v-if="cityOpts.length"> 
-    <my-tree-select  name="tree" label="下拉树" :options="cityOpts" :props="{'multiple': true, 'clearable': true, 'only-leaf': false, 'useOriginOpts': true, tree: {'checkStrictly': true}}"  >
+  <my-form ref="form" :model="model" @submit="handleSubmit"> 
+    <my-tree-select  v-if="cityOpts.length"  name="tree" label="下拉树" :options="cityOpts" :props="{'multiple': true, 'clearable': true, 'only-leaf': false, 'useOriginOpts': true, tree: {'checkStrictly': true}}"  >
     </my-tree-select>
-    <!-- <my-cascader name="city1" :options="cityOpts" root="100000" :use-origin-opts="true"  :props="{'clearable': true, 'props': { 'checkStrictly': true } }"></my-cascader>
+    <my-cascader name="city1" :options="cityOpts" root="100000" :use-origin-opts="true"  :props="{'clearable': true, 'props': { 'checkStrictly': true } }"></my-cascader>
     <my-cascader name="city2" :options="cityOpts" root="100000"  :use-origin-opts="true"  ></my-cascader> 
     <my-cascader name="city3" :options="cityOpts" root="100000"  :use-origin-opts="true"  ></my-cascader> 
     <my-cascader name="dept" :options="deptOpts" root="0" :use-origin-opts="true"  ></my-cascader>
     <my-cascader name="dept2" :options="deptOpts" root="0" :use-origin-opts="true"  ></my-cascader>
     <my-cascader name="dept3" :options="deptOpts" root="0" :use-origin-opts="true"  ></my-cascader>
-    <my-cascader name="dept4" :options="deptOpts" root="0" :use-origin-opts="true"  ></my-cascader> -->
+    <my-cascader name="dept4" :options="deptOpts" root="0" :use-origin-opts="true"  ></my-cascader>
   </my-form>
 </div>
 </template>
@@ -30,8 +30,8 @@ export default {
       cityOpts: [],
       model: {
         
-        tree: ['520500', '520526']
-        // city1: ['520000', '520400'],
+        tree: ['520500', '520526'],
+        city1: ['130000', '130200', '130209']
         // city2: ['130000', '130200', '130209']
       }
     }
@@ -56,10 +56,10 @@ export default {
     }
   },
   created() {
-    // setTimeout(() => {
+    setTimeout(() => {
       this.changeCityOpts()
       this.changeDeptOpts()
-    // }, 200) 
+    }, 200) 
   }, 
   mounted() {
     
