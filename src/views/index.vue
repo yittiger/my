eb48d8cbd6366635c49e36c59eee37db70377bb0
josephvong1008 <m15914375662@163.com>
2021-09-03 23:11:10 +0800

    20210903

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,8 +1,7 @@
 <template>
   <div >
     <router-link to="/login">去login 页面测试 router劝鉴</router-link><br/>
-    <router-link to="/modify-test/tree-select">测试tree-select仅末端可选功能</router-link><br/>
-    <router-link to="/modify-test/dict-test">全局字典</router-link> 
+   
   </div>
 </template>
 <script> 
