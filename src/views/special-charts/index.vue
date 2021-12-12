62caa957e557dddc02ecde31838a59a7401a5952
josephvong1008 <m15914375662@163.com>
2021-12-12 10:47:05 +0800

    202112121030

---
+++ src/views/special-charts/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>special
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  props: {
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  created() {
+  },
+  mounted() {
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/special-charts/linear-bar4.vue
@@ -0,0 +1,46 @@
+<template>
+   <div style="height:400px">
+     <my-dv-page target="parent" fit>
+        <my-dv-bar fit :columns="columns" :rows="rows" :settings="settings" :extend="extend" legend></my-dv-bar>
+     </my-dv-page>
+   </div>
+</template>
+<script>
+import {LinearGradient} from 'echarts/lib/util/graphic'
+import {colorData} from '$ui/utils/color'
+const colors = ['#74FCED', '#40D3F7'] 
+export default {
+  data() {
+    const barColor = colors.map((color) => {
+      const cData = colorData(color)
+      return new LinearGradient(0, 0, 0, 0.9, [
+        {
+          offset: 0,
+          color: `rgba(${cData._rgb}, 0.9)`
+        },
+        {
+          offset: 1,
+          color: `rgba(${cData._rgb}, 0.1)` 
+        }
+      ])
+    })
+    return {
+      columns: ['星期', '直接访问', '邮件营销', '联盟广告'],
+      rows: [
+          ['周一', 320, 302, 301],
+          ['周二', 120, 132, 101],
+          ['周三', 220, 182, 191],
+          ['周四', 150, 212, 201],
+          ['周五', 320, 132, 401]
+      ],
+      settings: {
+        stack: ['直接访问', '邮件营销'],
+        lines: ['联盟广告']
+      },
+      extend: {
+        color: [...barColor, '#5D67FA']
+      }
+    }
+  }
+}
+</script>
