6561d324ddc03fb8eb5abcc077e27633da9c3dbc
josephvong <m15914375662@163.com>
2021-12-13 10:48:54 +0800

    '202112131040'

--- src/views/modify-test/taginput.vue
+++ src/views/modify-test/taginput.vue
@@ -3,12 +3,8 @@
     <my-form @submit="handleSubmit">
       <my-tag-input name="tags" label="标签"></my-tag-input>
     </my-form>
-    标签2
-    <my-tag-input v-model="arr" :props="{allowCreate:false}" ></my-tag-input>
-    <el-button @click="test">test</el-button>
-    <!-- <my-checkbox v-model="check" :options="categories" width="100%" button></my-checkbox> -->
+     
 
-    <my-checkbox v-model="check" :group="true" :options="categories" :props="{border: true}"></my-checkbox>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -21,15 +17,7 @@ export default {
   },
   data() {
     return {
-      check: [],
-      check2: false,
-      arr: ['ccc', 'ddd'],
-      categories: [
-          { label: '种类一', value: 1 },
-          { label: '种类二', value: 2 },
-          { label: '种类三', value: 3 },
-          { label: '种类四', value: 4 }
-      ]
+      
     }
   },
   computed: {
@@ -37,10 +25,7 @@ export default {
   methods: {
     handleSubmit(model) {
       alert(`submit: ${JSON.stringify(model)}`)
-    },
-    test() {
-      alert(`submit: ${JSON.stringify(this.check)}`)
-    }
+    } 
   },
   created() {},
   mounted() {}
