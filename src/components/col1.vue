3d7c0db2390e26884a6bad2fa332919465f548ab
josephvong <m15914375662@163.com>
2022-02-07 17:14:19 +0800

    '202202071714'

---
+++ src/components/col1.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="table-origin" style="height: 100%">
+      <my-table :columns="columns" :data="list" fit></my-table>
+    </div>
+</template>
+
+<script>
+import Mock from 'mockjs'
+
+export default {
+  data() {
+    return {
+      columns: [
+          { type: 'index', label: '#' },
+          { label: '姓名', prop: 'name'},
+          { label: '身份证', prop: 'id'},
+          { label: '年龄', prop: 'age'},
+          { label: '地址', prop: 'address'},
+          { label: '日期', prop: 'date'}
+      ],
+      list: []
+    }
+  },
+  methods: {
+    mock(limit) {
+      return Mock.mock({
+        [`list|${limit}`]: [
+            {
+              id: '@id',
+              name: '@cname',
+              'age|10-100': 18,
+              address: '@ctitle',
+              date: '@date(yyyy-MM-dd)'
+            }
+        ]
+      })
+    }
+  },
+  created() {
+    this.list = this.mock(30).list
+  },
+  mounted() {
+    // this.$emit('on-listShow')
+  }
+}
+</script>
---
+++ src/views/d_huangjiping/test.vue
@@ -0,0 +1,63 @@
+<template>
+  <div style="height: 100%">
+    <my-tabs :tabs="tabs" position="top" fit>
+      <template v-slot="{tab}">
+        <div  style="height: 100%">
+          <col1 v-show="tab.name === 'tab1'"></col1>
+
+          <col2 v-show="tab.name === 'tab2'"></col2>
+        </div>   
+      </template>
+    </my-tabs>
+  </div>
+</template>
+<style lang="scss" scoped>
+</style>
+<script>
+
+import Col1 from '@/components/col1'
+import Col2 from '@/components/col2'
+export default {
+  mixins: [],
+  components: {Col1, Col2},
+  props: {
+  },
+  data() {
+    return {
+      tabs: [
+         {
+          name: 'tab1', 
+          label: '基本设置'
+          // component: Col1, 
+          // props: {}, 
+          // on: {
+          //   'on-listShow': this.colFirstShow
+          // },
+          // ref: 'col1'
+        },
+        {
+          name: 'tab2', 
+          label: '账号绑定'
+          // component: Col2, 
+          // props: {}, 
+          // on: {
+          //   'on-listShow': this.colFirstShow
+          // },
+          // ref: 'col2'
+        }
+         
+      ]
+    }
+  },
+  computed: {
+  },
+  methods: {
+    colFirstShow() {
+      console.log('col1 is show') 
+      
+    }
+  },
+  created() {},
+  mounted() {}
+}
+</script>
